a39c2dbe3c7c23dcfc80f447a5d05acdf479cc18
palakkkpal <palak.25174@ee.du.ac.in>
2026-01-16 11:01:19 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,38 @@
+"""
+algorithm : Arranging clothes in an almirah 
+
+STEP 1 : Start the algorithm.
+
+STEP 2 : Input the total n number of shelves available in the almirah.
+
+STEP 3 : Create a list of all clothes to be stored in the almirah.
+
+STEP 4 : Categorize the clothes into different types such as :
+          -Formal 
+          -casual
+	      -Traditional
+	      -seasonal
+
+STEP 5 : Determine the frequency of usage for each clothing category.
+
+STEP 6 : Sort the clothing categories based on frequency of use, from MOST frequently used to LEAST frequently used.
+
+STEP 7 : Assign shelves to each clothing category based on : frequency of use & Available shelf space
+
+STEP 8 : Allocate easily reachable shelves for frequently used clothes to minimize the time required to find them.
+
+STEP 9 : Allocate upper or lower shelves for less frequently used clothes.
+
+STEP 10: Arrange clothes neatly on each shelf by :
+	 -folding clothes properly 
+	 -stacking similar clothes together.
+	 -avoiding empty space to optimize space utilization.
+
+STEP 11: Ensure that clothes of the same category are stored together to reduce searching time.
+
+STEP 12: If required, label shelves with clothing categories for easy identification.  
+
+STEP 13: Verify that all clothes are placed properly and shelf space is optimally utilized.
+
+STEP 14: End the algorithm.
+"""
---
+++ 2.py
@@ -0,0 +1,38 @@
+"""
+
+Algorithm :  Arranging clothes in a luggage bag for a 7-day trip
+
+STEP 1 : Start the algorithm.
+
+STEP 2 : Input the duration of the trip i.e, 7 days.
+
+STEP 3 : Create a list of all clothes and accessories required for the trip.
+
+STEP 4 : Categorize the items based on activities such as : 
+		- daily 
+		- formal
+		- outdoor activity wear
+		- accessories nd toiletries
+
+STEP 5 : Identify delicate clothing items that require special protection
+
+STEP 6 : Separate heavy items such as shoes or jacket from light items
+
+STEP 7 : Roll casual nd daily wear clothes to save space.
+
+STEP 8 : Fold formal & delicate clothes carefully to avoid wrinkles nd damage.
+
+STEP 9 : Place heavy items at the bottom of luggage bag to maintain balance.
+
+STEP 10: Place folded formal nd delicate clothes above heavy items 
+
+STEP 11: Fill empty space withh rolled clothes to maximize space usage
+
+STEP 12: Keep frequently needed items near the top of the luggage bag for easy accessibility 
+
+STEP 13: Place accessories nd toiletries in separate pouches to avoiod mixing with clothes 
+
+STEP 14: Ensure all delicate items are properly protected.
+
+STEP 15: End the algorithm.
+"""
